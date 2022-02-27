d6c39a6d1a6724a310a51b33d6ed425a56ebf067
Abu <abut70243@gmail.com>
2022-02-27 20:43:26 +0600

    after besic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useRef } from 'react';
 
 function App() {
+  const nameRef = useRef<HTMLInputElement>(null)
+  const feedbackRef = useRef<HTMLInputElement>(null)
+  const ratingRef = useRef<HTMLInputElement>(null)
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+    console.log(nameRef.current?.value, feedbackRef.current?.value, ratingRef.current?.value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Feedback-App</h1><form onSubmit={handleSubmit}>
+        <input className='mb-2' required type="text" placeholder='name' ref={nameRef} />
+        <br />
+        <input className='mb-2' required type="text" placeholder='feedback' ref={feedbackRef} />
+        <br />
+        <input required type="number" placeholder='rating'  ref={ratingRef} />
+        <br />
+        <button type='submit' className='btn btn-outline-primary mt-2'>submit</button>
+      </form>
     </div>
   );
 }
